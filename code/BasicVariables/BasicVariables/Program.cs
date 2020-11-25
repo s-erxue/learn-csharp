7f866423acb714d81eff34268b5383fd4146c795
Eric Xue <eric.xy.xue@gmail.com>
2020-11-25 15:16:16 -0800

    Add win/variables

---
+++ code/BasicVariables/BasicVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BasicVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 42;
+            myInt = 123;
+            Console.WriteLine(myInt);
+        }
+    }
+}
