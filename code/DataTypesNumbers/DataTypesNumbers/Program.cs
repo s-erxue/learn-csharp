f9ae94a4bdd78ff06240224025ea9d691971b241
Eric Xue <eric.xy.xue@gmail.com>
2020-12-02 11:44:44 -0800

    Add win/numbers

---
+++ code/DataTypesNumbers/DataTypesNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataTypesNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int myInt = 42;
+            Console.WriteLine(myInt);
+
+            long myLong = 2147483648L;
+            Console.WriteLine(myLong);
+
+            float pi = 3.14F;
+            Console.WriteLine(pi);
+
+            double piAsDouble = 3.14159265D;
+            Console.WriteLine(piAsDouble);
+        }
+    }
+}
